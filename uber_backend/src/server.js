1edf968bc1ebee12db1660ae41e246f08f245e3d
Davila-II <daviladutau@gmail.com>
2026-02-14 23:33:35 +0100

    correction pour le crashdu serveur

--- uber_backend/src/server.js
+++ uber_backend/src/server.js
@@ -6,22 +6,22 @@ const authRoutes = require('./routes/authRoutes');
 
 const app = express();
 
-// Middlewares (les outils de base)
+// Middlewares
 app.use(cors());
-app.use(express.json()); // Pour que le serveur comprenne le format JSON
+app.use(express.json()); 
 
+// Routes
 app.use('/api/auth', authRoutes);
 
-// Une petite route pour tester si le serveur répond
+// Route de test
 app.get('/', (req, res) => {
   res.send('Le serveur Uber_CM fonctionne !');
 });
 
+// Configuration du Port pour Railway
 const PORT = process.env.PORT || 5000;
-server.listen(PORT, '0.0.0.0', () => {
-    console.log(`Serveur en ligne sur le port ${PORT}`);
-});
 
-app.listen(PORT, () => {
-  console.log(`Serveur lancé sur : http://localhost:${PORT}`);
+// Une seule écoute propre utilisant "app"
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`✅ Serveur Uber_CM lancé avec succès sur le port ${PORT}`);
 });
